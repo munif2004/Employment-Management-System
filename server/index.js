f5bba629c5400c450a32c4fcded5882e0d6f3809
A <A@gmail.com>
2025-08-04 06:23:50 -0700

    Remove deprecated mongoose options

--- server/index.js
+++ server/index.js
@@ -8,12 +8,9 @@ import dotenv from "dotenv";
 
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log("Error to Connect", err));
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Error to Connect", err));
 
 
 
